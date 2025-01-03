cb5966da89b053189bdf6b802c872f3de199d96f
Ahmed Moeen Serdah <aserdah@gmail.com>
2025-01-03 20:16:16 +0200

    Initial Commit

---
+++ CharactersTests/CharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersTests.swift
+//  CharactersTests
+//
+//  Created by Ahmed Serdah on 03/01/2025.
+//
+
+import Testing
+@testable import Characters
+
+struct CharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
